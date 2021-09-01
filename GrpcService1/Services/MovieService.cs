826de164bbfeff53c8de0b8722f35fe1c6849b49
Yurii Nazarenko <Yurii_Nazarenko@epam.com>
2021-09-01 19:41:04 +0300

    Renaming on the main entity

--- GrpcService1/Services/MovieService.cs
+++ GrpcService1/Services/MovieService.cs
@@ -4,37 +4,37 @@ using System.Collections.Generic;
 
 namespace GrpcService1.Services
 {
-    public class BookService
+    public class MovieService
     {
-        private readonly IMongoCollection<Movie> _books;
+        private readonly IMongoCollection<Movie> _movies;
 
-        public BookService(IMoviesDatabaseSettings settings)
+        public MovieService(IMoviesDatabaseSettings settings)
         {
             var client = new MongoClient(settings.ConnectionString);
             var database = client.GetDatabase(settings.DatabaseName);
 
-            _books = database.GetCollection<Movie>(settings.MoviesCollectionName);
+            _movies = database.GetCollection<Movie>(settings.MoviesCollectionName);
         }
 
         public List<Movie> Get() =>
-            _books.Find(book => true).ToList();
+            _movies.Find(book => true).ToList();
 
         public Movie Get(string id) =>
-            _books.Find<Movie>(book => book.Id == id).FirstOrDefault();
+            _movies.Find<Movie>(movie => movie.Id == id).FirstOrDefault();
 
-        public Movie Create(Movie book)
+        public Movie Create(Movie movie)
         {
-            _books.InsertOne(book);
-            return book;
+            _movies.InsertOne(movie);
+            return movie;
         }
 
         public void Update(string id, Movie bookIn) =>
-            _books.ReplaceOne(book => book.Id == id, bookIn);
+            _movies.ReplaceOne(movie => movie.Id == id, bookIn);
 
         public void Remove(Movie bookIn) =>
-            _books.DeleteOne(book => book.Id == bookIn.Id);
+            _movies.DeleteOne(movie => movie.Id == bookIn.Id);
 
         public void Remove(string id) =>
-            _books.DeleteOne(book => book.Id == id);
+            _movies.DeleteOne(movie => movie.Id == id);
     }
 }
