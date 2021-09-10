b5e1d6c2806833a93a7080f32c9eb656ee180b1a
T-REX-XP <T-REX-XP@ya.ru>
2021-09-10 11:42:25 +0300

    Fix mapping

--- grpcService/Profiles/MoviesResponseProfile.cs
+++ grpcService/Profiles/MoviesResponseProfile.cs
@@ -10,7 +10,6 @@ namespace GrpcService1.Profiles
     {
         public MoviesResponseProfile()
         {
-            CreateMap<MoviesResponse, IEnumerable<MovieGRPC>>().ForMember(d => d, m => m.MapFrom(x => x.Movies));
             CreateMap<IEnumerable<MovieGRPC>, MoviesResponse>().ForMember(d => d.Movies, m => m.MapFrom(x => x));
         }
     }
